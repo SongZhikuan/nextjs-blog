dd56473e12acbaccfb58ce4a6223077ac1033226
songzhikuan <850993286@qq.com>
2021-03-07 17:32:21 +0800

    删除测试文章

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -61,7 +61,6 @@ export function getAllPostIds() {
 export async function getPostData(id: string) {
     const fullPath = path.join(postsDirectory, `${id}.md`)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
-
     // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents)
     // Use remark to convert markdown into HTML string
